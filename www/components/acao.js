a9fe6848b46673ad841ed31d11e7b602cdb4e80c
carloshenriqds18 <carlos.henriqds18@hotmail.com>
2019-11-13 03:09:20 +0900

    quase tudo pronto

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,9 +1,19 @@
 // This is a JavaScript file
 
+$(document).ready(function() {
+    M.updateTextFields();
+});
+
 $(document).on("click","#listarV",function(){
  
   $(location).attr('href', 'listar.html');
 })
+
+$(document).on("click","#pagamentoV",function(){
+ 
+  $(location).attr('href', 'pagamento.html');
+})
+
 
 $(document).on("click", "#cadastrarV", function(){
   var parametros  = {
@@ -11,6 +21,7 @@ $(document).on("click", "#cadastrarV", function(){
     "placaVeiculo":$("#placaVeiculo").val(),
     "marcaVeiculo":$("#marcaVeiculo").val(),
     "modeloVeiculo":$("#modeloVeiculo").val(),
+    "dataEntrada":$("#dataEntrada").val(),
     "tipoV":$("#tipoV").val(),
     };
 
@@ -25,6 +36,7 @@ $(document).on("click", "#cadastrarV", function(){
           $("#placaVeiculo").val("");
           $("#marcaVeiculo").val("");
           $("#modeloVeiculo").val("");
+          $("#dataEntrada").val("");
           $("#tipoV").val("");
       },
       error: function(data){
@@ -33,22 +45,79 @@ $(document).on("click", "#cadastrarV", function(){
   });
 });
 
-function carregaLista(){
+$(document).on("click", "#pesquisaV",function(){
+      var parametro ={
+      "placa":$("#placaVeiculo").val()
+    };
+    
     $.ajax({
     type:"post", // Como vai enviar os dados
-    url:"https://gerenciarestacionamento2.000webhostapp.com/listar.php", // pra onde vai enviar
+    url:"https://gerenciarestacionamento2.000webhostapp.com/listar.php",
+    data: parametro, // pra onde vai enviar
     dataType:"json", // o que eu vou enviar
     // caso sucesso
-    success: function(data){ 
-      var itemlista = "";
-      $.each(data.pizzas, function(i,dados){
-        itemlista += "<option value="+dados.codigo+" title"+dados.nome+" alt"+dados.placa+">"+dados.marca+ dados.modelo + dados.tipo + "</option>"
-      });
-      $("#lista").html(itemlista);
-    },
+    success: function(data){
+        $("#codigoVeiculo").val(data.veiculos.codigo);
+        $("#nomeProprietario").val(data.veiculos.nome);
+        $("#marcaVeiculo").val(data.veiculos.marca);
+        $("#modeloVeiculo").val(data.veiculos.modelo);
+        $("#dataEntrada").val(data.veiculos.entrada);   
+        $("#tipoV").val(data.veiculos.tipo);                
+  },
     // caso erro
     error: function(data){
       navigator.notification.alert("Erro ao buscar registros!");
     }
   });
+});
+
+$document.on("change", "#dataSaida", function(){
+function preencheHora(){
+    var hora = "";
+    for(var x = 0; x <= 23; x++){
+      if(x <= 9){
+        hora+="<option value="+x+">0"+x+"</option>";
+      }else{
+        hora+="<option value="+x+">"+x+"</option>";
+      }
+    }
+    $("#dataEntrada").html(hora);
+    $("#dataSaida").html(hora);
 }
+
+function preencheMinuto(){
+  var minuto = "";
+    for(var x = 0; x <= 59; x++){
+      if(x <= 9){
+        minuto+="<option value="+x+">0"+x+"</option>";
+      }else{
+        minuto+="<option value="+x+">"+x+"</option>";
+      }
+    }
+    $("#minutoEntrada").html(minuto);
+    $("#minutoSaida").html(minuto);
+}
+var total = ((horaSaida + (minutoSaida/60)) - (horaEntrada + (minutoEntrada/60))) * valorHora;
+
+})
+
+$(document).on("click", "#pagamentoV", function(){
+  var parametros  = {
+    "dataSaida":$("#dataSaida").val(),
+    };
+
+      $.ajax({
+      type:"post", //Forma de envio
+      url: "https://gerenciarestacionamento2.000webhostapp.com/cadastro.php",  //Local de envio
+      data: parametros, // Os dados enviados
+      success: function(data){ //Função de Sucesso de Cadastro
+        
+          navigator.notification.alert(data);
+          $("#dataSaida").val("");
+
+      },
+      error: function(data){
+          navigator.notification.alert("Erro no pagamento");
+      }
+  });
+});
